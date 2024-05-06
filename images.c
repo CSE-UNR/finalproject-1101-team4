30fd600107e7dccc5fbab4426b0cc6b7167342a5
1105-Trujillo-Brooke <brookeatr2005@gmail.com>
2024-05-06 05:07:33 +0000

    added to menus and combine code the save image functions not working tho.

--- images.c
+++ images.c
@@ -3,24 +3,41 @@
 
 #include <stdio.h>
 
+#define MAX_WIDTH 100
+#define MAX_HEIGHT 100
+#define MAX_FILE_LENGTH 20
 
 //prototypes
-void loadImage();
-void displayImage();
-void edditingMenu();
+void loadImage(char imagePixels[][MAX_WIDTH], char fileName[MAX_FILE_LENGTH], int* imgLength, int* imgWidth);
+void displayImage(char imagePixels[][MAX_WIDTH], char fileName[MAX_FILE_LENGTH], int imgSize, int imgLength, int imgWidth);
+void editingMenu(int editChoice);
 void cropImage();
 void brightImage();
 void dimImage();
-void saveToFile();
+void saveToFile(char imagePixels[][MAX_WIDTH], int imgLength, int imgWidth);
 
 //main function
 int main(){
+	//stores each pixel as a value in a 2d array
+	char imageInfo[MAX_HEIGHT][MAX_WIDTH];
+	
+	//resolution of the image
+	int imgRes;
+	
+	//int variables for saving the size of the user image.
+	int imgLength=0, imgWidth=0;
+	
+	//name of file being edited
+	char fileName[MAX_FILE_LENGTH];
 	
 	int menuChoice, editChoice;
-	
+	char input;
 	FILE *fp;	
+	
+	printf("Welcome to the Image Processor!\n");
 	do{
 		//Main Menu
+		
 		printf("\n**ERINSTAGRAM**\n");
 		printf("1: Load image\n");
 		printf("2: Display image\n");
@@ -32,19 +49,27 @@ int main(){
 		switch(menuChoice){
 			//loads image 
 			case 1:
-			loadImage();
+				printf("Okay, what file do you want to load?\n");
+				scanf("%s", fileName);
+				
+				loadImage(imageInfo, fileName, &imgLength, &imgWidth);
+				
+				imgRes= imgLength* imgWidth;
 			break;
 			//displays image
 			case 2:
-			displayImage();
+				displayImage(imageInfo, fileName, imgRes, imgLength, imgWidth);
 			break;
 			//brings up edit menu
 			case 3:
-			edditingMenu();
+				editingMenu(editChoice);
 			break;
 			//closes the program
 			case 0:
 				printf("\nGoodbye!\n\n");
+			break;
+			default:
+			printf("Invalid input. Please try again.");
 			break;
 		}
 	}while (menuChoice != 0);
@@ -52,13 +77,75 @@ return 0;
 }
 
 //definitions
-void loadImage(){
+void loadImage(char imagePixels[][MAX_WIDTH], char fileName[MAX_FILE_LENGTH], int* imgLength, int* imgWidth){
+	
+	//file pointer for file IO
+	FILE* imgLoad;
+	char temp[MAX_WIDTH];
+	
+	imgLoad= fopen(fileName, "r");
+	
+	//for loop that assigns each image pixel to an address in the array and the "brightness" value
+	for(int i=0; i<= MAX_HEIGHT; i++){
+	printf("Row %d \n", i);
+		if(fscanf(imgLoad, "%s", &temp[i])==1){
+			for(int j=0; temp[j]!= '\0'; j++){
+					if(i=0){
+						*imgWidth= j;
+					}
+					
+					fscanf(imgLoad, "%c", &imagePixels[i][j]);
+					
+		
+					switch(imagePixels[i][j]){
+				
+						case'0':
+							imagePixels[i][j]= ' ';
+							break;
+				
+						case '1':
+							imagePixels[i][j]= '.';
+							break;
+						case '2':
+							imagePixels[i][j]= 'o';
+							break;
+						case '3':
+							imagePixels[i][j]= 'O';
+							break;
+						case '4':
+							imagePixels[i][j]= '0';
+							break;
+						default:
+							imagePixels[i][j]= 'X';
+							break;
+					}
+			printf("%c", imagePixels[i][j]);
+			}
+			*imgLength= i+1;
+	}
+	}
+	fclose(imgLoad);	
 }
-void displayImage(){
+
+void displayImage(char imagePixels[][MAX_WIDTH], char fileName[MAX_FILE_LENGTH], int imgSize, int imgLength, int imgWidth){
+
+	printf("Image Name: %s\n", fileName);
+	printf("This image is %d by %d pixels\n", imgLength, imgWidth);	
+	printf("The total amount of pixels in this image is %d\n", imgSize);
+	
+	for(int i=0; i<imgLength; i++){
+		for(int j=1; j<imgWidth; j++){
+			printf("%c", imagePixels[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
 }
-void edditingMenu(int editChoice){
+
+void editingMenu(int editChoice){
+char input;
 do{
-		//edditing Menu
+		//editing Menu
 		printf("\n\n**EDITING**\n");
 		printf("1: Crop image\n");
 		printf("2: Dim image\n");
@@ -72,17 +159,35 @@ do{
 			//crop Image 
 			case 1:
 				cropImage();
+				printf("Would you like to save the file? (y/n) ");
+				scanf(" %c", &input);
+				if (input != 'n'){
+					saveToFile(imageInfo, imgLength, imgWidth);
+				}
 			break;
 			//bright Image
 			case 2:
 				brightImage();
+				printf("Would you like to save the file? (y/n) ");
+				scanf(" %c", &input);
+				if (input != 'n'){
+					saveToFile(imageInfo, imgLength, imgWidth);
+				}
 			break;
 			//dimImage
 			case 3:
 				dimImage();
+				printf("Would you like to save the file? (y/n) ");
+				scanf(" %c", &input);
+				if (input != 'n'){
+					saveToFile(imageInfo, imgLength, imgWidth);
+				}
 			break;
 			//back to main menue
 			case 0:
+			break;
+			default:
+			printf("Invalid input. Please try again.");
 			break;
 		}
 	}while (editChoice != 0);
@@ -93,6 +198,7 @@ void brightImage(){
 }
 void dimImage(){
 }
-void saveToFile(){
+void saveToFile(char imagePixels[][MAX_WIDTH], int imgLength, int imgWidth){
+
 }
 
